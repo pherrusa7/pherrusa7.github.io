e471d71c122f9a128ee77061b42d72bb78c2b493
Pedro Herruzo <hs.pedro@hotmail.com>
2019-11-26 18:00:19 +0100

    my website v1

---
+++ heading.js
@@ -0,0 +1,64 @@
+document.write(`
+
+    <h1 class="blog-name pt-lg-4 mb-0"><a href="index.html"> Pedro Herruzo Sánchez</a></h1>
+        
+	    <nav class="navbar navbar-expand-lg navbar-dark" >
+           
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div id="navigation" class="collapse navbar-collapse flex-column" >
+				<div class="profile-section pt-3 pt-lg-0">
+				    <img class="profile-image mb-3 rounded-circle mx-auto" src="assets/images/profile.png" alt="image" >			
+					
+					<div class="bio mb-3">Machine Learning Consultant & Data Scientist at <a href="https://www.seat.es/">SEAT,S.A.</a>, Industrial PhD Student in AI & Smart Mobility at the <a href="https://www.dama.upc.edu/es/el-equipo-1">Polytechnic University of Catalonia</a>.<br></div><!--//bio-->
+
+					<u >
+			            <i >
+			            	<a href="https://www.seat.es/">
+			            		<img src="my_web/images/cv/SEAT_Logo_from_2017.png" alt="SEAT" width="51px">
+			            	</a>
+			            </i>
+
+			            <i >
+			            	<a href="https://www.dama.upc.edu/es/el-equipo-1">
+			            		<img src="my_web/images/cv/upc_n.png" alt="UPC" width="51px">
+			            	</a>
+			            </i>
+			        </u><!--//social-list-->
+			        
+					<ul class="social-list list-inline py-3 mx-auto">
+			            <li class="list-inline-item"><a href="https://twitter.com/HerruzoPedro"><i class="fab fa-twitter fa-fw"></i></a></li>
+			            <li class="list-inline-item"><a href="https://www.linkedin.com/in/pherrusa7/"><i class="fab fa-linkedin-in fa-fw"></i></a></li>
+			            <li class="list-inline-item"><a href="https://github.com/pherrusa7"><i class="fab fa-github-alt fa-fw"></i></a></li>
+			            <li class="list-inline-item"><a href="https://scholar.google.com/citations?user=uvXH2PYAAAAJ&hl=en"><i class="fab fa-google fa-fw"></i></a></li>
+			            <li class="list-inline-item"><a href="https://www.quora.com/profile/Pedro-Herruzo"><i class="fab fa-quora fa-fw"></i></a></li>
+			            <li class="list-inline-item">
+			            	<a href="https://dblp.uni-trier.de/pers/hd/h/Herruzo:Pedro">
+			            		<i class="fas fa-graduation-cap"></i>
+			            	</a>
+			            </li>
+			        </ul><!--//social-list-->
+			        <hr> 
+				</div><!--//profile-section-->
+				
+				<ul class="navbar-nav flex-column text-left">
+					<li class="nav-item active">
+					    <a class="nav-link" href="index.html"><i class="fas fa-home fa-fw mr-2"></i>Blog Home <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item">
+					    <a class="nav-link" href="blog-post.html"><i class="fas fa-bookmark fa-fw mr-2"></i>Blog Post</a>
+					</li>
+					<li class="nav-item">
+					    <a class="nav-link" href="about.html"><i class="fas fa-user fa-fw mr-2"></i>About Me</a>
+					</li>
+				</ul>
+				
+				<div class="my-2 my-md-3">
+				    <a class="btn btn-primary" href="https://themes.3rdwavemedia.com/" target="_blank">Get in Touch</a>
+				</div>
+			</div>
+		</nav>
+
+`);
